62aac7078af31b6342e6f55b0a6709a01950df6a
Natpelizari <natypelizari@gmail.com>
2023-05-07 18:31:58 -0300

    first commit

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,36 @@
+function leiaMais(){
+    var maisTexto = document.getElementById("mais");
+
+    if(maisTexto.style.display === "none"){
+        maisTexto.style.display = "block";
+    }
+    else{
+        maisTexto.style.display = "none";
+    }
+
+}
+
+function leiaMais1(){
+    var maisTexto1 = document.getElementById("mais1");
+
+    if(maisTexto1.style.display === "none"){
+        maisTexto1.style.display = "block";
+    }
+    else{
+        maisTexto1.style.display = "none";
+    }
+
+}
+
+function leiaMais2(){
+    var maisTexto2 = document.getElementById("mais2");
+
+    if(maisTexto2.style.display === "none"){
+        maisTexto2.style.display = "block";
+    }
+    else{
+        maisTexto2.style.display = "none";
+    }
+
+}
+
